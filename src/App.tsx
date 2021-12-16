80a95873b453444f48ad416c04b996bb52143488
Micah Hauge <micahhauge@gmail.com>
2021-12-15 19:45:09 -0600

    add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@ import { ReactElement } from 'react';
 import { fetchMovieData } from './utils/fetchMovieData';
 
 function App(): ReactElement {
-  return <p>Hello world</p>;
+  // return <p>Hello world</p>;
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 }
 
 export { App };
