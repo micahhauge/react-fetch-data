cfa03bd0b21692efebfa53198b0db15bf9b20e89
Micah Hauge <micahhauge@gmail.com>
2021-12-22 11:06:47 -0600

    add oldApp

---
+++ src/OldApp.tsx
@@ -0,0 +1,42 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { MovieList, LoadingPlaceholder, ErrorNotification } from './components';
+import { FetchMoviesResponse, Movie } from './types';
+
+function App() {
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const [loadingMovies, setLoadingMovies] = useState(true);
+  const [errorLoadingMovies, setErrorLoadingMovies] = useState(false);
+
+  useEffect(() => {
+    axios
+      .get<FetchMoviesResponse>('http://www.omdbapi.com/', {
+        params: {
+          apiKey: '87d9551a',
+          type: 'movie',
+          s: 'Lord of the Rings',
+        },
+      })
+      .then((response) => {
+        setMovies(response.data.Search);
+        setLoadingMovies(false);
+      })
+      .catch((error) => {
+        setLoadingMovies(false);
+        setErrorLoadingMovies(true);
+        throw error;
+      });
+  }, []);
+
+  if (errorLoadingMovies) {
+    return <ErrorNotification />;
+  }
+
+  if (loadingMovies) {
+    return <LoadingPlaceholder />;
+  }
+
+  return <MovieList movies={movies} />;
+}
+
+export { App };
