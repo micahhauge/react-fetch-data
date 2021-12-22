5102103d96925a72f27572ea23b4165045bfa75a
Micah Hauge <micahhauge@gmail.com>
2021-12-22 10:49:59 -0600

    update loading placeholder

--- src/components/LoadingPlaceholder.tsx
+++ src/components/LoadingPlaceholder.tsx
@@ -9,9 +9,9 @@ function LoadingPlaceholder(): ReactElement {
         <div className="flex-1 space-y-6 py-1">
           <div className="h-2 bg-gray-700 rounded"></div>
           <div className="space-y-3">
-            <div className="grid grid-cols-3 gap-4">
-              <div className="h-2 bg-gray-700 rounded col-span-2"></div>
+            <div className="grid grid-cols-5 gap-4">
               <div className="h-2 bg-gray-700 rounded col-span-1"></div>
+              <div className="h-2 bg-gray-700 rounded col-span-4"></div>
             </div>
             <div className="h-2 bg-gray-700 rounded"></div>
           </div>
